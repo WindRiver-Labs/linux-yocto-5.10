858a21834ecbe7a24dbc3a4ab0e5f89fee691e13
Peter Chen <peter.chen@nxp.com>
2017-02-03 11:16:21 +0800

    MLK-13851-1 usb: chipidea: otg: delete the workaround code
    commit 21b67cda90abce58d4dd9e514e51a113abc34b57 from
    https://source.codeaurora.org/external/imx/linux-imx
    
    After commit 49670184289e ("usb: chipidea: otg: change workqueue
    ci_otg as freezable"), we have fixed the bug that ID removed
    wakeup (ID: 0->1) will lock up system resume, we delete the
    workaround code in this commit.
    
    Signed-off-by: Peter Chen <peter.chen@nxp.com>
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>

--- drivers/usb/chipidea/otg.c
+++ drivers/usb/chipidea/otg.c
@@ -3,6 +3,7 @@
  * otg.c - ChipIdea USB IP core OTG driver
  *
  * Copyright (C) 2013-2016 Freescale Semiconductor, Inc.
+ * Copyright 2017 NXP
  *
  * Author: Peter Chen
  */
@@ -194,12 +195,6 @@ void ci_handle_id_switch(struct ci_hdrc *ci)
 			 * switch occurs during system suspend.
 			 */
 			usb_gadget_vbus_disconnect(&ci->gadget);
-
-		while (ci_hdrc_host_has_device(ci)) {
-                       enable_irq(ci->irq);
-                       usleep_range(10000, 15000);
-                       disable_irq_nosync(ci->irq);
-               }
 
 		ci_role_stop(ci);
 
