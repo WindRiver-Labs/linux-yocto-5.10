573417101c2f8ef005dce8782d959c2ca66a5fdc
Shengjiu Wang <shengjiu.wang@freescale.com>
2017-07-12 18:19:25 +0800

    MLK-15960-1: ASoC: fsl_sai: update fifo_depth for different platform
    commit f4a5a728876b3ba4fd4dc74de1f3dc9dc000077f from
    https://source.codeaurora.org/external/imx/linux-imx
    
    The fifo_depth is changed to 64 in imx8qm/imx8qxp, in imx8mq, the
    fifo_depth is 128. which is mentioned in their ADD.
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@freescale.com>
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>

--- sound/soc/fsl/fsl_sai.c
+++ sound/soc/fsl/fsl_sai.c
@@ -1260,6 +1260,8 @@ static int fsl_sai_runtime_suspend(struct device *dev)
 {
 	struct fsl_sai *sai = dev_get_drvdata(dev);
 
+	regcache_cache_only(sai->regmap, true);
+
 	if (sai->mclk_streams & BIT(SNDRV_PCM_STREAM_CAPTURE))
 		clk_disable_unprepare(sai->mclk_clk[sai->mclk_id[0]]);
 
