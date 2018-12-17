0957f585d640187e194cc97de55588f269cd52a5
Shengjiu Wang <shengjiu.wang@nxp.com>
2018-12-17 16:19:32 +0800

    MLK-18182: ASoC: fsl_asrc: increase the retry times
    commit 37baed62f6318b44eb386dd69eba06fd2a00dede from
    https://source.codeaurora.org/external/imx/linux-imx
    
    When 3 asrc pair are working in parallel, there will be
    error log randomly
    
    output DMA task timeout
    fsl-asrc 2034000.asrc: Pair B: failed to process buffer: -62
    
    The reason is that the initialization is not finished for
    some pair, the prefill data is failed to be input in FIFO.
    
    The patch is to increase the retry times to make sure that
    initialization is success.
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>

--- sound/soc/fsl/fsl_asrc.c
+++ sound/soc/fsl/fsl_asrc.c
@@ -559,7 +559,7 @@ static void fsl_asrc_start_pair(struct fsl_asrc_pair *pair)
 {
 	struct fsl_asrc *asrc = pair->asrc;
 	enum asrc_pair_index index = pair->index;
-	int reg, retry = 10, i;
+	int reg, retry = 50, i;
 
 	/* Enable the current pair */
 	regmap_update_bits(asrc->regmap, REG_ASRCTR,
@@ -571,6 +571,9 @@ static void fsl_asrc_start_pair(struct fsl_asrc_pair *pair)
 		regmap_read(asrc->regmap, REG_ASRCFG, &reg);
 		reg &= ASRCFG_INIRQi_MASK(index);
 	} while (!reg && --retry);
+
+	if (retry == 0)
+		pair_warn("initialization is not finished\n");
 
 	/* Make the input fifo to ASRC STALL level */
 	regmap_read(asrc->regmap, REG_ASRCNCR, &reg);
@@ -1259,7 +1262,7 @@ static int fsl_asrc_runtime_resume(struct device *dev)
 	int i, ret;
 	u32 asrctr;
 	u32 reg;
-	int retry = 10;
+	int retry = 50;
 
 	ret = clk_prepare_enable(asrc->mem_clk);
 	if (ret)
@@ -1302,6 +1305,9 @@ static int fsl_asrc_runtime_resume(struct device *dev)
 		regmap_read(asrc_priv->regmap, REG_ASRCFG, &reg);
 		reg = (reg >> ASRCFG_INIRQi_SHIFT(0)) & 0x7;
 	} while (!(reg == ((asrctr & 0xE) >> 1)) && --retry);
+
+	if (retry == 0)
+		dev_warn(dev, "initialization is not finished\n");
 
 	return 0;
 
