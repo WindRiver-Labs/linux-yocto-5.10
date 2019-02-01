099fe8269b5fa7a6110bc65291efa7176687dbf4
Lukasz Bartosik <lbartosik@marvell.com>
2019-02-01 19:34:50 +0530

    octeontx2-af: don't register CPT AF interrupts
    commit 9d1fd5dc03f696706bd6803a2b1344e1a68656e7 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Don't register CPT AF interrupts if CPT block is not
    implemented on a given platform.
    
    Change-Id: I3fcf69f2cae14979eb3037e8391789800d74f34e
    Signed-off-by: Lukasz Bartosik <lbartosik@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.c
@@ -2741,9 +2741,11 @@ static int rvu_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 
 	rvu_setup_rvum_blk_revid(rvu);
 
-	err = rvu_cpt_register_interrupts(rvu);
-	if (err)
-		goto err_irq;
+	if (is_block_implemented(rvu->hw, BLKADDR_CPT0)) {
+		err = rvu_cpt_register_interrupts(rvu);
+		if (err)
+			goto err_irq;
+	}
 
 	err = rvu_policy_init(rvu);
 	if (err)
