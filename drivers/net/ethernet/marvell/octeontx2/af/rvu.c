24b4ebd774f080e5e2f620356678678bd8ce3008
Pavan Nikhilesh <pbhagavatula@marvell.com>
2019-02-01 14:49:15 +0530

    octeontx2-af: Add TIM unit support.
    commit 8d3537a8e7feb6fc139775f7c8c3798333fec2a1 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Add TIM (Timer) unit support to AF driver that involves initializing and
    configuring TIM and its rings through mailbox.
    
    Change-Id: Ib42edb126554a5cc78af14f96fa966d5d2202fad
    Signed-off-by: Andrew Pinski <apinski@marvell.com>
    Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>
    [Kevin: Just some minor context mods in order to port to linux-yocto]
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.c
@@ -664,7 +664,7 @@ static void rvu_free_hw_resources(struct rvu *rvu)
 		kfree(block->lf.bmap);
 	}
 
-	/* Free MSIX bitmaps */
+	/* Free MSIX and TIM bitmaps */
 	for (id = 0; id < hw->total_pfs; id++) {
 		pfvf = &rvu->pf[id];
 		kfree(pfvf->msix.bmap);
@@ -947,6 +947,10 @@ init:
 		goto cgx_err;
 
 	err = rvu_sso_init(rvu);
+	if (err)
+		goto cgx_err;
+
+	err = rvu_tim_init(rvu);
 	if (err)
 		goto cgx_err;
 
