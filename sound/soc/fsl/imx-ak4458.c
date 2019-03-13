23dff39f3f5c3ce28165d128603f3a446e25d3ab
Shengjiu Wang <shengjiu.wang@nxp.com>
2019-03-13 16:29:45 +0800

    MLK-21106: ASoC: imx-ak4458: Fix channel not supported in tdm & daisy chain
    commit 533604f38375f437cdf11ad79d048afb3e12096a from
    https://source.codeaurora.org/external/imx/linux-imx
    
    The channel num (9 - 15) should not be supported in tdm & daisy chain
    mode for there is two dataline, this channel number can't be
    symmetrically distributed on two dataline (the first one is fixed to
    be 8 channel)
    
    Fixes commit 8d29874365c3 ("MLK-17817-2: ASoC: imx-ak4458: enable 16
    channels in TDM mode")
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>
    (cherry picked from commit 0bc47b31660efd3dbbeee7e51fca6f75e7ecb69f)
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>

--- sound/soc/fsl/imx-ak4458.c
+++ sound/soc/fsl/imx-ak4458.c
@@ -98,7 +98,7 @@ static const u32 ak4458_channels[] = {
 };
 
 static const u32 ak4458_channels_tdm[] = {
-	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+	1, 2, 3, 4, 5, 6, 7, 8, 16,
 };
 
 static unsigned long ak4458_get_mclk_rate(struct snd_pcm_substream *substream,
@@ -142,7 +142,7 @@ static int imx_aif_hw_params(struct snd_pcm_substream *substream,
 				struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
+	struct snd_soc_dai *cpu_dai = asoc_rtd_to_cpu(rtd, 0);
 	struct snd_soc_card *card = rtd->card;
 	struct device *dev = card->dev;
 	struct imx_ak4458_data *data = snd_soc_card_get_drvdata(card);
@@ -181,7 +181,7 @@ static int imx_aif_hw_params(struct snd_pcm_substream *substream,
 	}
 
 	for (i = 0; i < rtd->num_codecs; i++) {
-		struct snd_soc_dai *codec_dai = rtd->codec_dais[i];
+		struct snd_soc_dai *codec_dai = asoc_rtd_to_codec(rtd, i);
 
 		ret = snd_soc_dai_set_fmt(codec_dai, fmt);
 		if (ret) {
@@ -329,9 +329,7 @@ static int imx_ak4458_probe(struct platform_device *pdev)
 	}
 
 	priv->codec_conf[0].name_prefix = "0";
-	priv->codec_conf[0].of_node = codec_np_0;
 	priv->codec_conf[1].name_prefix = "1";
-	priv->codec_conf[1].of_node = codec_np_1;
 
 	ak4458_codecs[0].of_node = codec_np_0;
 	ak4458_codecs[1].of_node = codec_np_1;
