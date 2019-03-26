c9364bda70dbef33afa46360aa744adaa33acfba
Jerin Jacob <jerinj@marvell.com>
2019-03-26 12:04:12 +0530

    octeontx2-af: add nix error af interrupt handlers
    commit 7bd7af921aa4de5239292ca778370b9816883613 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Added debug messages for NIX_AF_RVU_INT, NIX_AF_ERR_INT and NIX_AF_RAS
    error AF interrupts.
    
    Change-Id: Ifccda17e7f77ae726bfdd2f4157d4a86bfec689d
    Signed-off-by: Jerin Jacob <jerinj@marvell.com>
    Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.h
@@ -580,6 +580,8 @@ int rvu_get_nixlf_count(struct rvu *rvu);
 void rvu_nix_lf_teardown(struct rvu *rvu, u16 pcifunc, int blkaddr, int npalf);
 int nix_get_nixlf(struct rvu *rvu, u16 pcifunc, int *nixlf, int *nix_blkaddr);
 int nix_update_bcast_mce_list(struct rvu *rvu, u16 pcifunc, bool add);
+int rvu_nix_register_interrupts(struct rvu *rvu);
+void rvu_nix_unregister_interrupts(struct rvu *rvu);
 
 /* NPC APIs */
 int rvu_npc_init(struct rvu *rvu);
