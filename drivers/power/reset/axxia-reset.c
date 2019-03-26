951d385c13462d6328fc1f7bce740ed21b1dce4e
John Jacques <john.jacques@intel.com>
2019-03-26 13:06:55 +0200

    power: reset: Add support for Axxia DDR Retention reset
    commit  16363a35b4c3bbb2e4d8fa67cd5afcb7dd0e71de from
    https://github.com/axxia/linux-yocto.git v5.10/standard/preempt-rt/axxia-dev/base
    
    Add support for Axxia X9/AXM56xx reset function which gets called
    when Linux calls arm_pm_restart, for doing machine specific restart.
    
    Signed-off-by: John Jacques <john.jacques@intel.com>
    Signed-off-by: SangeethaRao <sangeetha.rao@intel.com>
    Signed-off-by: Gary McGee <gary.mcgee@intel.com>
    Signed-off-by: Marek Majtyka <marekx.majtyka@intel.com>
    Signed-off-by: Per Hallsmark <per.hallsmark@windriver.com>
    Signed-off-by: Charlie Paul <charlie.paul@windriver.com>
    Signed-off-by: Daniel Dragomir <daniel.dragomir@windriver.com>
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/power/reset/axxia-reset.c
+++ drivers/power/reset/axxia-reset.c
@@ -1,9 +1,9 @@
-// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (C) 2021 INTEL Corporation
 /*
  * Reset driver for Axxia devices
- *
- * Copyright (C) 2014 LSI
  */
+
 #include <linux/init.h>
 #include <linux/err.h>
 #include <linux/io.h>
@@ -13,30 +13,101 @@
 #include <linux/notifier.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
+#include <linux/proc_fs.h>
 #include <linux/reboot.h>
 #include <linux/regmap.h>
+#include <asm/system_misc.h>
 
-#define SC_CRIT_WRITE_KEY	0x1000
-#define SC_LATCH_ON_RESET	0x1004
-#define SC_RESET_CONTROL	0x1008
-#define   RSTCTL_RST_ZERO	(1<<3)
-#define   RSTCTL_RST_FAB	(1<<2)
-#define   RSTCTL_RST_CHIP	(1<<1)
-#define   RSTCTL_RST_SYS	(1<<0)
-#define SC_EFUSE_INT_STATUS	0x180c
-#define   EFUSE_READ_DONE	(1<<31)
+#define SC_PSCRATCH            0x00dc
+#define SC_CRIT_WRITE_KEY      0x2000
+#define SC_RESET_CONTROL       0x2008
+#define   RSTCTL_RST_CHIP       BIT(1)
+#define   RSTCTL_RST_SYS	BIT(0)
 
 static struct regmap *syscon;
+
+static int ddr_retention_enabled;
+
+#ifdef CONFIG_POWER_RESET_AXXIA_DDR_RETENTION
+static void (*saved_arm_pm_restart)
+(enum reboot_mode reboot_mode, const char *cmd);
+#endif
+
+void
+initiate_retention_reset(void)
+{
+	if (ddr_retention_enabled == 0) {
+		pr_info("DDR Retention Reset is Not Enabled\n");
+		return;
+	}
+
+	if (WARN_ON(!syscon))
+		return;
+
+	/* set retention reset bit in pscratch */
+	regmap_update_bits(syscon, SC_PSCRATCH, 1, 1);
+
+	/* trap into secure monitor to do the reset */
+#ifdef CONFIG_POWER_RESET_AXXIA_DDR_RETENTION
+	saved_arm_pm_restart(0, NULL);
+#else
+	machine_restart(NULL);
+#endif
+}
+EXPORT_SYMBOL(initiate_retention_reset);
+
+#ifdef CONFIG_POWER_RESET_AXXIA_DDR_RETENTION
+static void axxia_pm_restart(enum reboot_mode reboot_mode, const char *cmd)
+{
+	initiate_retention_reset();
+}
+#endif
+
+static ssize_t
+axxia_ddr_retention_trigger(struct file *file, const char __user *buf,
+			    size_t count, loff_t *ppos)
+{
+	initiate_retention_reset();
+	return 0;
+}
+
+static const struct file_operations axxia_ddr_retention_proc_ops = {
+	.write      = axxia_ddr_retention_trigger,
+	.llseek     = noop_llseek,
+};
+
+void
+axxia_ddr_retention_init(void)
+{
+	/*
+	 * this feature is only meaningful on ASIC systems,
+	 * but for now we allow it on simulator
+	 */
+
+	/*
+	 * if (of_find_compatible_node(NULL, NULL, "axxia,axm5500-amarillo")) {
+	 */
+
+	if (1) {
+		/* Create /proc entry. */
+		if (!proc_create("driver/axxia_ddr_retention_reset",
+				 0200, NULL, &axxia_ddr_retention_proc_ops)) {
+			pr_info("Failed to register DDR retention proc entry\n");
+		} else {
+			ddr_retention_enabled = 1;
+			pr_info("DDR Retention Reset Initialized\n");
+		}
+	} else {
+		pr_info("DDR Retention Reset is Not Available\n");
+	}
+}
 
 static int axxia_restart_handler(struct notifier_block *this,
 				 unsigned long mode, void *cmd)
 {
 	/* Access Key (0xab) */
 	regmap_write(syscon, SC_CRIT_WRITE_KEY, 0xab);
-	/* Select internal boot from 0xffff0000 */
-	regmap_write(syscon, SC_LATCH_ON_RESET, 0x00000040);
-	/* Assert ResetReadDone (to avoid hanging in boot ROM) */
-	regmap_write(syscon, SC_EFUSE_INT_STATUS, EFUSE_READ_DONE);
+
 	/* Assert chip reset */
 	regmap_update_bits(syscon, SC_RESET_CONTROL,
 			   RSTCTL_RST_CHIP, RSTCTL_RST_CHIP);
@@ -64,11 +135,16 @@ static int axxia_reset_probe(struct platform_device *pdev)
 	if (err)
 		dev_err(dev, "cannot register restart handler (err=%d)\n", err);
 
+#ifdef CONFIG_POWER_RESET_AXXIA_DDR_RETENTION
+	saved_arm_pm_restart = arm_pm_restart;
+	arm_pm_restart = axxia_pm_restart;
+#endif
+
 	return err;
 }
 
 static const struct of_device_id of_axxia_reset_match[] = {
-	{ .compatible = "lsi,axm55xx-reset", },
+	{ .compatible = "intel,axm56xx-reset",},
 	{},
 };
 MODULE_DEVICE_TABLE(of, of_axxia_reset_match);
@@ -77,12 +153,13 @@ static struct platform_driver axxia_reset_driver = {
 	.probe = axxia_reset_probe,
 	.driver = {
 		.name = "axxia-reset",
-		.of_match_table = of_match_ptr(of_axxia_reset_match),
+		.of_match_table = of_axxia_reset_match,
 	},
 };
 
 static int __init axxia_reset_init(void)
 {
+	axxia_ddr_retention_init();
 	return platform_driver_register(&axxia_reset_driver);
 }
 device_initcall(axxia_reset_init);
