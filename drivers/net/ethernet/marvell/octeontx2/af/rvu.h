7df231e28364b2b46f44800b2fca33d52937cb73
Sunil Kumar Kori <skori@marvell.com>
2019-06-06 12:17:14 +0530

    octeontx2-af: CGX DMAC filtering support
    commit a269429853b005d71965e935f73c67524562ecf6 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    This patch implements following mbox messages:
    1. Message to add mac address into DMAC filter table.
    2. Message to delete mac address from DMAC filter table.
    3. Message to get maximum number of DMAC filters per lmac.
    
    Using above messages, CGX can be configured with DMAC filters
    so that packet can be dropped/accepted at CGX level for defined
    destination mac addresses.
    
    CGX DMAC filter table are logically divided among all the attached LMACs
    to it as given below:
    1. CGX0 is having 1 LMAC then maximum supported filters are 32 for LMAC
       ranging from 0 to 31. First index is reserved for default MAC address.
    2. CGX1 is having 4 LMACs then maximum supported filters are 8 for each
       LMACs ranging as given below:
    
       a. Index 0-7 for LMAC0 and 0 is reserved for default MAC address.
       b. Index 8-15 for LMAC1 and 8 is reserved for default MAC address.
       c. Index 16-23 for LMAC2 and 16 is reserved for default MAC address.
       d. Index 24-31 for LMAC3 and 24 is reserved for default MAC address.
    
    Change-Id: Ie9406accda04a8a1feeb74216d5e46570bfdfde2
    Signed-off-by: Sunil Kumar Kori <skori@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.h
@@ -575,6 +575,7 @@ int rvu_cgx_exit(struct rvu *rvu);
 void *rvu_cgx_pdata(u8 cgx_id, struct rvu *rvu);
 int rvu_cgx_config_rxtx(struct rvu *rvu, u16 pcifunc, bool start);
 void rvu_cgx_enadis_rx_bp(struct rvu *rvu, int pf, bool enable);
+void rvu_cgx_disable_dmac_entries(struct rvu *rvu, u16 pcifunc);
 int rvu_cgx_start_stop_io(struct rvu *rvu, u16 pcifunc, bool start);
 int rvu_cgx_nix_cuml_stats(struct rvu *rvu, void *cgxd, int lmac_id, int index,
 			   int rxtxflag, u64 *stat);
