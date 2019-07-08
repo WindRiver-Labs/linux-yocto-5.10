c4c756e13b00ff21274d11052684a9c38fc9c636
Fugang Duan <fugang.duan@nxp.com>
2019-07-08 14:06:14 +0800

    net: fec: add imx8mq and imx8qm new versions support
    commit 819700d6f78264de196680c351d9e4a0a79c3c9b from
    https://source.codeaurora.org/external/imx/linux-imx
    
    The ENET of imx8mq and imx8qm are basically the same as imx6sx,
    but they have new features support based on imx6sx, like:
    - imx8mq: supports IEEE 802.3az EEE standard.
    - imx8qm: supports RGMII mode delayed clock.
    
    Signed-off-by: Fugang Duan <fugang.duan@nxp.com>
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>

--- drivers/net/ethernet/freescale/fec.h
+++ drivers/net/ethernet/freescale/fec.h
@@ -456,6 +456,18 @@ struct bufdesc_ex {
  */
 #define FEC_QUIRK_HAS_FRREG		(1 << 16)
 
+/* i.MX8MQ ENET IP version add new feature to support IEEE 802.3az EEE
+ * standard. For the transmission, MAC supply two user registers to set
+ * Sleep (TS) and Wake (TW) time.
+ */
+#define FEC_QUIRK_HAS_EEE              (1 << 17)
+/* i.MX8QM ENET IP version add new feture to generate delayed TXC/RXC
+ * as an alternative option to make sure it works well with various PHYs.
+ * For the implementation of delayed clock, ENET takes synchronized 250MHz
+ * clocks to generate 2ns delay.
+ */
+#define FEC_QUIRK_DELAYED_CLKS_SUPPORT (1 << 18)
+
 /* Some FEC hardware blocks need the MMFR cleared at setup time to avoid
  * the generation of an MII event. This must be avoided in the older
  * FEC blocks where it will stop MII events being generated.
