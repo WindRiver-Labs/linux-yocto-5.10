1e595eaa79f2695aeda92bd3e2843da788dda45b
Peng Fan <peng.fan@nxp.com>
2019-10-18 16:29:36 +0800

    clk: imx8mm/mn/mq: add imx_clk_init_on
    commit 754fce578f4def981d606e858ae18f4927907658 from
    https://source.codeaurora.org/external/imx/linux-imx
    
    When we need to support dual linux with jailhouse, there is no clock
    controller in 2nd inmate linux cell, it relys on the first linux to
    configure the clock ready and on. So we add those clocks required for
    the 2nd linux in dts to make them prepare enabled, and pass
    clk_ignore_unused to the 1st linux, then the 1st linux will not gated
    off the clocks. So the 2nd linux could use IPs without touching clocks.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>

--- drivers/clk/imx/clk-imx8mq.c
+++ drivers/clk/imx/clk-imx8mq.c
@@ -282,6 +282,32 @@ static const int uart_clk_ids[] = {
 };
 static struct clk **uart_hws[ARRAY_SIZE(uart_clk_ids) + 1];
 
+static int __init imx_clk_init_on(struct device_node *np,
+                                 struct clk * const clks[])
+{
+        u32 *array;
+        int i, ret, elems;
+
+        elems = of_property_count_u32_elems(np, "init-on-array");
+        if (elems < 0)
+                return elems;
+        array = kcalloc(elems, sizeof(elems), GFP_KERNEL);
+        if (IS_ERR_OR_NULL(array))
+                return PTR_ERR(array);
+
+        ret = of_property_read_u32_array(np, "init-on-array", array, elems);
+        if (ret)
+                return ret;
+
+        for (i = 0; i < elems; i++) {
+                ret = clk_prepare_enable(clks[array[i]]);
+                if (ret)
+                        pr_err("clk_prepare_enable failed %d\n", array[i]);
+        }
+
+        return 0;
+}
+
 static int imx8mq_clocks_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
@@ -623,6 +649,9 @@ static int imx8mq_clocks_probe(struct platform_device *pdev)
 
 		uart_hws[i] = &hws[index]->clk;
 	}
+
+	/* enable all the clocks just for bringup */
+	imx_clk_init_on(np, clks);
 
 	clk_set_parent(clks[IMX8MQ_CLK_CSI1_CORE], clks[IMX8MQ_SYS1_PLL_266M]);
 	clk_set_parent(clks[IMX8MQ_CLK_CSI1_PHY_REF], clks[IMX8MQ_SYS2_PLL_1000M]);
