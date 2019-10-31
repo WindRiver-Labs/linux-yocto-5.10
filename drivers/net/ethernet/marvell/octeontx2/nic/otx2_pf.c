35a6244320f765a1deb004a1689ba26bbfa73f41
Sunil Goutham <sgoutham@marvell.com>
2019-10-31 15:58:24 +0530

    octeontx2-pf: Allow setting VF mac and vlan only when PF is UP
    commit 48040e84a699a16b1a9421d92fb6da0c064a3ae7 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    To install NPC MCAM rules to support VF MAC and VF VLAN settings
    PF needs to know ingress hw channel number. This info is sent to
    PF by AF in response to NIXLF_ALLOC mbox ie NIXLF init request.
    
    So skip 'set vf vlan' and 'set vf mac' if PF interface is not UP.
    
    Change-Id: Ib8d0c5c84296aa22090d39038055e769668ac07f
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/18079
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Tested-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
@@ -2001,6 +2001,9 @@ static int otx2_set_vf_mac(struct net_device *netdev, int vf, u8 *mac)
 	struct pci_dev *pdev = pf->pdev;
 	struct otx2_vf_config *config;
 
+	if (!netif_running(netdev))
+		return -EAGAIN;
+
 	if (vf >= pci_num_vf(pdev))
 		return -EINVAL;
 
@@ -2047,6 +2050,9 @@ static int otx2_set_vf_vlan(struct net_device *netdev, int vf, u16 vlan, u8 qos,
 	struct otx2_nic *pf = netdev_priv(netdev);
 	struct pci_dev *pdev = pf->pdev;
 	struct otx2_vf_config *config;
+
+	if (!netif_running(netdev))
+		return -EAGAIN;
 
 	if (vf >= pci_num_vf(pdev))
 		return -EINVAL;
