8b6f4a71c4d621928b54cb7146bc26f0b51d2186
Suneel Garapati <sgarapati@marvell.com>
2019-11-11 14:09:49 -0800

    drivers: mtd: spi-nor: update 4B_OPCODES flag for n25q00 and mx66l1g45g
    commit fecda6c6673fa2a7fd414b332fc69f63a5394344 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Use SPI_NOR_4B_OPCODES flag for n25q00 and mx66l1g45g flash parts so as
    to avoid mode change in flash. With mode change to 4byte address, on 8xx
    platforms, reboot causes firmware boot to fail as it expects default
    3byte address mode for flash operations. This flag avoids change of
    address mode with use of 4Byte address opcodes for flash.
    
    Change-Id: Ic87874f8149c5787821634c4dd5e5f7b7f3cf647
    Signed-off-by: Suneel Garapati <sgarapati@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/18706
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Tested-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>
    [RQ: Format to fit new kernel code.]
    Signed-off-by: Ruiqiang Hao <ruiqiang.hao@windriver.com>

--- drivers/mtd/spi-nor/micron-st.c
+++ drivers/mtd/spi-nor/micron-st.c
@@ -63,7 +63,7 @@ static const struct flash_info st_parts[] = {
 			      SECT_4K | USE_FSR | SPI_NOR_QUAD_READ |
 			      SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB |
 			      SPI_NOR_4BIT_BP | SPI_NOR_BP3_SR_BIT6 |
-			      NO_CHIP_ERASE) },
+			      NO_CHIP_ERASE | SPI_NOR_4B_OPCODES)},
 	{ "n25q00a",     INFO(0x20bb21, 0, 64 * 1024, 2048,
 			      SECT_4K | USE_FSR | SPI_NOR_QUAD_READ |
 			      NO_CHIP_ERASE) },
