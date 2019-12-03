25b0e28bdbd24743e1fc74b07fc292110df62117
Nava kishore Manne <nava.manne@xilinx.com>
2019-12-03 01:03:37 -0800

    firmware: xilinx: Added pm api functions for RSA and SHA
    commit  ed5b5afb90215836086a2fb1bda38e8cbefb9061 from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v5.4
    
    This patch adds PM APIs to provided access to xilsecure
    library to calculate SHA3 hash on the data or
    decrypt the data by using RSA public or private
    keys respectively.
    
    Signed-off-by: Durga Challa <vnsldurg@xilinx.com>
    Signed-off-by: Nava kishore Manne <nava.manne@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    State: pending
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>
    [Quanyang: declare zynqmp_pm_rsa/sha_hash as global since the mainline commit b9b3a8be28b3
    ("firmware: xilinx: Remove eemi ops for get_api_version")]

--- include/linux/firmware/xlnx-zynqmp.h
+++ include/linux/firmware/xlnx-zynqmp.h
@@ -70,8 +70,12 @@ enum pm_api_id {
 	PM_FPGA_LOAD,
 	PM_FPGA_GET_STATUS,
 	PM_GET_CHIPID = 24,
+	/* ID 25 is been used by U-boot to process secure boot images */
+	/* Secure library generic API functions */
+	PM_SECURE_SHA = 26,
+	PM_SECURE_RSA,
 	/* Pin control API functions */
-	PM_PINCTRL_REQUEST = 28,
+	PM_PINCTRL_REQUEST,
 	PM_PINCTRL_RELEASE,
 	PM_PINCTRL_GET_FUNCTION,
 	PM_PINCTRL_SET_FUNCTION,
@@ -403,6 +407,8 @@ int zynqmp_pm_set_requirement(const u32 node, const u32 capabilities,
 			      const enum zynqmp_pm_request_ack ack);
 int zynqmp_pm_aes_engine(const u64 address, u32 *out);
 int zynqmp_pm_efuse_access(const u64 address, u32 *out);
+int zynqmp_pm_sha_hash(const u64 address, const u32 size, const u32 flags);
+int zynqmp_pm_rsa(const u64 address, const u32 size, const u32 flags);
 int zynqmp_pm_pinctrl_request(const u32 pin);
 int zynqmp_pm_pinctrl_release(const u32 pin);
 int zynqmp_pm_pinctrl_get_function(const u32 pin, u32 *id);
@@ -568,6 +574,14 @@ static inline int zynqmp_pm_set_boot_health_status(u32 value)
 	return -ENODEV;
 }
 static inline int zynqmp_pm_efuse_access(const u64 address, u32 *out)
+{
+	return -ENODEV;
+}
+static inline int zynqmp_pm_sha_hash(const u64 address, const u32 size, const u32 flags)
+{
+	return -ENODEV;
+}
+static inline int zynqmp_pm_rsa(const u64 address, const u32 size, const u32 flags)
 {
 	return -ENODEV;
 }
