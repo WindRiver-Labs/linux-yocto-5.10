08cf6c1a69419ffe96f9b447d646576e836d6e9a
Michal Simek <michal.simek@xilinx.com>
2019-12-03 01:03:43 -0800

    firmware: xilinx: Add AFI ioctl support
    commit  74a48ec4d32ce0b3ab5cd1b87f9fd58b23a2127c from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v5.4
    
    Just wire AFI ioctls.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    State: pending
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>
    [Quanyang: Because of mainline commit 426c8d85df7a ("firmware: xilinx: Use APIs instead of IOCTLs"),
    use the new API zynqmp_pm_afi instead of eemi_ops->ioctl]

--- include/linux/firmware/xlnx-zynqmp.h
+++ include/linux/firmware/xlnx-zynqmp.h
@@ -149,6 +149,7 @@ enum pm_ioctl_id {
 	IOCTL_READ_PGGS = 15,
 	/* Set healthy bit value */
 	IOCTL_SET_BOOT_HEALTH_STATUS = 17,
+	IOCTL_AFI,
 };
 
 enum pm_query_id {
@@ -475,6 +476,7 @@ int zynqmp_pm_write_ggs(u32 index, u32 value);
 int zynqmp_pm_read_ggs(u32 index, u32 *value);
 int zynqmp_pm_write_pggs(u32 index, u32 value);
 int zynqmp_pm_read_pggs(u32 index, u32 *value);
+int zynqmp_pm_afi(u32 index, u32 value);
 int zynqmp_pm_system_shutdown(const u32 type, const u32 subtype);
 int zynqmp_pm_set_boot_health_status(u32 value);
 #else
@@ -616,6 +618,10 @@ static inline int zynqmp_pm_write_pggs(u32 index, u32 value)
 	return -ENODEV;
 }
 static inline int zynqmp_pm_read_pggs(u32 index, u32 *value)
+{
+	return -ENODEV;
+}
+static inline int zynqmp_pm_afi(u32 index, u32 value)
 {
 	return -ENODEV;
 }
