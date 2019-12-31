c86fc3b61422087ffd99fa4229157d4f340adedd
Peng Fan <peng.fan@nxp.com>
2019-12-31 16:51:08 +0800

    MLK-23178 clk: imx: imx8mp: remove memrepair clock
    commit bd0fd2d5d9822225a8087b119b22ae9fcefd23ec from
    https://source.codeaurora.org/external/imx/linux-imx
    
    memrepair clock needs to be on for HDMIMIX memrepair before
    de-assert HDMIMIX reset. If the clock is run time off by Linux,
    the memrepair will not be functional. Since there is no software
    user in Linux side, let's remove this clock and leave it
    default on.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>

--- drivers/clk/imx/clk-imx8mp.c
+++ drivers/clk/imx/clk-imx8mp.c
@@ -376,10 +376,6 @@ static const char * const imx8mp_media_ldb_sels[] = {"osc_24m", "sys_pll2_333m",
 						     "clk_ext2", "audio_pll2_out",
 						     "video_pll1_out", };
 
-static const char * const imx8mp_memrepair_sels[] = {"osc_24m", "sys_pll2_100m", "sys_pll1_80m",
-							"sys_pll1_800m", "sys_pll2_1000m", "sys_pll3_out",
-							"clk_ext3", "audio_pll2_out", };
-
 static const char * const imx8mp_pcie2_ctrl_sels[] = {"osc_24m", "sys_pll2_250m", "sys_pll2_200m",
 						      "sys_pll1_266m", "sys_pll1_800m", "sys_pll2_500m",
 						      "sys_pll2_333m", "sys_pll3_out", };
@@ -642,7 +638,6 @@ static int imx8mp_clocks_probe(struct platform_device *pdev)
 	hws[IMX8MP_CLK_MEDIA_DISP1_PIX] = imx8m_clk_hw_composite("media_disp1_pix", imx8mp_media_disp1_pix_sels, ccm_base + 0xbe00);
 	hws[IMX8MP_CLK_MEDIA_CAM2_PIX] = imx8m_clk_hw_composite("media_cam2_pix", imx8mp_media_cam2_pix_sels, ccm_base + 0xbe80);
 	hws[IMX8MP_CLK_MEDIA_LDB] = imx8m_clk_hw_composite("media_ldb", imx8mp_media_ldb_sels, ccm_base + 0xbf00);
-	hws[IMX8MP_CLK_MEMREPAIR] = imx8m_clk_hw_composite_critical("mem_repair", imx8mp_memrepair_sels, ccm_base + 0xbf80);
 	hws[IMX8MP_CLK_PCIE2_CTRL] = imx8m_clk_hw_composite("pcie2_ctrl", imx8mp_pcie2_ctrl_sels, ccm_base + 0xc000);
 	hws[IMX8MP_CLK_PCIE2_PHY] = imx8m_clk_hw_composite("pcie2_phy", imx8mp_pcie2_phy_sels, ccm_base + 0xc080);
 	hws[IMX8MP_CLK_MEDIA_MIPI_TEST_BYTE] = imx8m_clk_hw_composite("media_mipi_test_byte", imx8mp_media_mipi_test_byte_sels, ccm_base + 0xc100);
