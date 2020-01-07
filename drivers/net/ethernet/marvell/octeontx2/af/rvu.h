5d0ad73d6233728287fe21a232710abc50365eb8
hariprasad <hkelam@marvell.com>
2020-01-07 17:12:16 +0530

    octeontx2-af: Put CGX LMAC also in Higig2 mode
    commit 6eda370a7ddada1814a8b42b516e811b293f10dc from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Currently upon user request to enable Higig2 mode, only NPC packet
    parsing related settings are done. This patch adds config to put
    CGX LMAC also in Higig2 mode. Actual HW config is done by firmware,
    so send a request to FW to get the config done.
    
    Other changes
    - Higig2 flow control enable/disable support.
    - CGX HW doesn't support timestamping when in Higig2 mode, so
      made PTP and Higig2 settings mutually exclusive.
    
    Change-Id: Id524b965f81835bc6364a4d17c025471e0cf74ff
    Signed-off-by: hariprasad <hkelam@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/21181
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.h
@@ -596,6 +596,8 @@ int rvu_cgx_exit(struct rvu *rvu);
 void *rvu_cgx_pdata(u8 cgx_id, struct rvu *rvu);
 int rvu_cgx_config_rxtx(struct rvu *rvu, u16 pcifunc, bool start);
 void rvu_cgx_enadis_rx_bp(struct rvu *rvu, int pf, bool enable);
+void rvu_cgx_enadis_higig2(struct rvu *rvu, int pf, bool enable);
+bool rvu_cgx_is_higig2_enabled(struct rvu *rvu, int pf);
 void rvu_cgx_disable_dmac_entries(struct rvu *rvu, u16 pcifunc);
 int rvu_cgx_start_stop_io(struct rvu *rvu, u16 pcifunc, bool start);
 int rvu_cgx_nix_cuml_stats(struct rvu *rvu, void *cgxd, int lmac_id, int index,
@@ -629,6 +631,7 @@ void rvu_nix_lf_teardown(struct rvu *rvu, u16 pcifunc, int blkaddr, int npalf);
 int nix_get_nixlf(struct rvu *rvu, u16 pcifunc, int *nixlf, int *nix_blkaddr);
 int nix_update_bcast_mce_list(struct rvu *rvu, u16 pcifunc, bool add);
 void rvu_nix_reset_mac(struct rvu_pfvf *pfvf, int pcifunc);
+bool rvu_nix_is_ptp_tx_enabled(struct rvu *rvu, u16 pcifunc);
 
 /* NPC APIs */
 int rvu_npc_init(struct rvu *rvu);
