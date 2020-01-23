19096d9649f0102afa7319df86c5b640eaf56f37
Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
2020-01-23 19:50:28 +0530

    usb: xhci: Add workaround for fixing ep stream ring hang issue
    commit  d0e355e7ab98cfb870119c2146c33472b2a60d7d from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v5.10
    
    Dwc3 host controller has an issue with BULK IN stream rings where
    it stops processing the TD's present in the endpoint stream ring
    (once in a while) even after ringing the stream DoorBell register.
    Since host controller doesn't process TD's in stream ring, no transfer
    events are generated by the controller, resulting in a hang issue.
    
    This patch solves the above said problem by adding a timeout handler for
    every BULK IN stream ring. After timer expires, timeout handler will be
    called and from that stop command on the same endpoint stream ring will
    be issued and urb will be given back with urb->status set to -EAGAIN.
    The UAS driver checks the urb status as -EAGAIN and re-submit the same
    urb which was timedout.
    
    NOTE: As we have already added support for usb Host streaming, this fix is
    required to fix the hang issue that occurs when UAS capable device is
    connected. This fix is expected to change after the HW team comes with an
    alternate workaround for this issue.
    
    Signed-off-by: Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Piyush Mehta <piyush.mehta@xilinx.com>
    State: pending
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1618,6 +1618,9 @@ struct xhci_ring {
 	enum xhci_ring_type	type;
 	bool			last_td_was_short;
 	struct radix_tree_root	*trb_address_map;
+	struct timer_list	stream_timer;
+	bool			stream_timeout_handler;
+	struct xhci_hcd		*xhci;
 };
 
 struct xhci_erst_entry {
@@ -1884,6 +1887,7 @@ struct xhci_hcd {
 #define XHCI_DISABLE_SPARSE	BIT_ULL(38)
 #define XHCI_SG_TRB_CACHE_SIZE_QUIRK	BIT_ULL(39)
 #define XHCI_NO_SOFT_RETRY	BIT_ULL(40)
+#define XHCI_STREAM_QUIRK	BIT_ULL(41) /* FIXME this is wrong */
 #define XHCI_EP_CTX_BROKEN_DCS	BIT_ULL(42)
 
 	unsigned int		num_active_eps;
@@ -2138,6 +2142,7 @@ void xhci_cleanup_stalled_ring(struct xhci_hcd *xhci, unsigned int slot_id,
 			       unsigned int ep_index, unsigned int stream_id,
 			       struct xhci_td *td);
 void xhci_stop_endpoint_command_watchdog(struct timer_list *t);
+void xhci_stream_timeout(struct timer_list *unused);
 void xhci_handle_command_timeout(struct work_struct *work);
 
 void xhci_ring_ep_doorbell(struct xhci_hcd *xhci, unsigned int slot_id,
