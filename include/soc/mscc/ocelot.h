7f593207e9e5ee7f1f5a348dd19f351cf5496626
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2020-02-11 15:33:46 +0800

    LF-457: ocelot: tsn: clean preempt interrupt status
    commit 68430e200e6de56731e5acc8c85d6e987a13db72 from
    https://source.codeaurora.org/external/imx/linux-imx
    
    The INTB interrupt is used both for 1588 interrupt and preemption status
    change interrupt on each port. So clean preempt status interrupt in IRQ
    handle function. Without handling it, driver may get interrupt storm.
    
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
    Reviewed-by: Po Liu <Po.Liu@nxp.com>
    (cherry picked from commit 0df3452eb1dec497ed75a7f804142e30972601b3)
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>

--- include/soc/mscc/ocelot.h
+++ include/soc/mscc/ocelot.h
@@ -815,6 +815,7 @@ int ocelot_rtag_parse_enable(struct ocelot *ocelot, u8 port);
 int ocelot_dscp_set(struct ocelot *ocelot, int port,
                    bool enable, const u8 dscp_ix,
                    struct tsn_qos_switch_dscp_conf *c);
+void ocelot_preempt_irq_clean(struct ocelot *ocelot);
 
 void ocelot_port_set_maxlen(struct ocelot *ocelot, int port, size_t sdu);
 int ocelot_get_max_mtu(struct ocelot *ocelot, int port);
