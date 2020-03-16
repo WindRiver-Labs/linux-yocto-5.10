37c6f6fd0ccaee334e08375d35b9524cee314b5f
Michal Simek <michal.simek@xilinx.com>
2020-03-16 13:34:39 +0100

    usb: dwc3: Add support for clock disabling during suspend
    commit  6d0468b837bc2b3cce625f59904d137e63637b5d from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v5.4
    
    In dwc3 usb controller, when remote wakeup feature is enabled and
    the controller is in U3 state (suspended), it requires suspend_clk
    to be active to detect wakeup signalling driven on the link.
    For all other cases usb clocks can be disabled once the controller
    enters U3 state.
    
    This patch adds the logic for disabling usb clocks when remote
    wakeup feature is not enabled.
    
    Signed-off-by: Piyush Mehta <piyush.mehta@xilinx.com>
    Signed-off-by: Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: pending
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -1072,6 +1072,7 @@ struct dwc3_scratchpad_array {
  * @dis_split_quirk: set to disable split boundary.
  * @imod_interval: set the interrupt moderation interval in 250ns
  *			increments or 0 to disable.
+ * @is_d3: set if the controller is in d3 state
  */
 struct dwc3 {
 	struct work_struct	drd_work;
@@ -1268,6 +1269,7 @@ struct dwc3 {
 	unsigned		dis_split_quirk:1;
 
 	u16			imod_interval;
+	bool			is_d3;
 };
 
 #define INCRX_BURST_MODE 0
@@ -1456,6 +1458,13 @@ u32 dwc3_core_fifo_space(struct dwc3_ep *dep, u8 type);
 	 dwc->version_type >= _ip##_VERSIONTYPE_##_from &&		\
 	 (!(_ip##_VERSIONTYPE_##_to) ||					\
 	  dwc->version_type <= _ip##_VERSIONTYPE_##_to))
+
+#if IS_ENABLED(CONFIG_USB_DWC3_OF_SIMPLE)
+void dwc3_simple_wakeup_capable(struct device *dev, bool wakeup);
+#else
+void dwc3_simple_wakeup_capable(struct device *dev, bool wakeup)
+{ ; }
+#endif
 
 bool dwc3_has_imod(struct dwc3 *dwc);
 
