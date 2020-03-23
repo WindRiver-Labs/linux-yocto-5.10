468e8c86efd72be3a3c7286ea28da83af4b3e8d1
Peng Fan <peng.fan@nxp.com>
2020-03-23 16:23:37 +0800

    MLK-23754-14 remoteproc: elf_loader: drop unneedded memset
    commit 00815a39affe5bffc11949c312cd6c3ecf8f98e6 from
    https://source.codeaurora.org/external/imx/linux-imx
    
    The memset is actually not needed and it could cause kernel dump
    on i.MX8M platform, because TCM is device memory in i.MX8M,
    memset on ARM64 has dc zva instruction. This instruction
    will trigger alignment abort to device memory per ARMv8 Doc.
    
    Reviewed-by: Richard Zhu <hongxing.zhu@nxp.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>

--- drivers/remoteproc/remoteproc_elf_loader.c
+++ drivers/remoteproc/remoteproc_elf_loader.c
@@ -216,15 +216,6 @@ int rproc_elf_load_segments(struct rproc *rproc, const struct firmware *fw)
 		if (filesz)
 			memcpy(ptr, elf_data + offset, filesz);
 
-		/*
-		 * Zero out remaining memory for this segment.
-		 *
-		 * This isn't strictly required since dma_alloc_coherent already
-		 * did this for us. albeit harmless, we may consider removing
-		 * this.
-		 */
-		if (memsz > filesz)
-			memset(ptr + filesz, 0, memsz - filesz);
 	}
 
 	return ret;
