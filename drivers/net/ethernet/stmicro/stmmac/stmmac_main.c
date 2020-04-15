b5d7f2e22f6deb5176fd93bd161a9206564c4d6a
Song, Yoong Siang <yoong.siang.song@intel.com>
2020-04-15 19:36:10 +0800

    net: stmmac: enable wakeup capability only when Network Proxy Agent is ready
    commit 5391643bb6f17db8dbf916dc1c21773601236a77 from
    https://github.com/intel/linux-intel-lts.git
    
    Introduce stmmac_netproxy_wakeup_enable() callback function so that
    Network Proxy driver can call to enable wakeup system capability
    of stmmac driver when Network Proxy Agent is ready.
    
    Suggested-by: Ong Boon Leong <boon.leong.ong@intel.com>
    Signed-off-by: Song, Yoong Siang <yoong.siang.song@intel.com>
    Signed-off-by: Yongxin Liu <yongxin.liu@windriver.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -7196,7 +7196,6 @@ int stmmac_dvr_probe(struct device *device,
 	if (priv->plat->has_netproxy) {
 		dev_info(priv->device, "Network Proxy supported\n");
 		device_set_wakeup_capable(priv->device, 1);
-		device_set_wakeup_enable(priv->device, 1);
 	}
 #endif
 
