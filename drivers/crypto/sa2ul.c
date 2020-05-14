ff2ecffad769ec51e5ad332d32da3e25b3851e99
Tero Kristo <t-kristo@ti.com>
2020-05-14 10:28:44 +0300

    crypto: sa2ul: disable crypto engines during failed probe
    commit 7e829edd4ec340c6f412f7e3debfaf148a9637dd from
    git://git.ti.com/ti-linux-kernel/ti-linux-kernel.git
    
    Crypto engines must be disabled under sa2ul if probe fails and we do
    pm_runtime disable. Otherwise sa2ul denies power transition and the PSC
    gets stuck, resulting in a crash during the next iteration of the
    deferred probing for the driver.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>
    Signed-off-by: Xulin Sun <xulin.sun@windriver.com>

--- drivers/crypto/sa2ul.c
+++ drivers/crypto/sa2ul.c
@@ -2352,11 +2352,6 @@ static int sa_ul_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	sa_init_mem(dev_data);
-	ret = sa_dma_init(dev_data);
-	if (ret)
-		goto disable_pm_runtime;
-
 	spin_lock_init(&dev_data->scid_lock);
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	saul_base = devm_ioremap_resource(dev, res);
@@ -2367,6 +2362,11 @@ static int sa_ul_probe(struct platform_device *pdev)
 	    SA_EEC_TRNG_EN;
 
 	writel_relaxed(val, saul_base + SA_ENGINE_ENABLE_CONTROL);
+
+	sa_init_mem(dev_data);
+	ret = sa_dma_init(dev_data);
+	if (ret)
+		goto disable_pm_runtime;
 
 	sa_register_algos(dev);
 
@@ -2388,6 +2388,8 @@ release_dma:
 	dma_pool_destroy(dev_data->sc_pool);
 
 disable_pm_runtime:
+	writel_relaxed(0, saul_base + SA_ENGINE_ENABLE_CONTROL);
+
 	pm_runtime_put_sync(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
 
