94564e9e1fa9a2c0d2632bc8f3cccc7bcf31a05d
Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
2020-07-01 22:32:20 +0530

    spi: spi-zynqmp-gqspi: Do hardware initialization later in probe
    commit  bad54eadf99cedbbb4c579320f1cfcc34a953b48 from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v5.4
    
    In probe the qspi controller is configured to operate at high frequencies.
    This configuration is done in the hardware initialization function.
    To achieve this the driver needs to set the clock and data tap delay
    values as per the operating frequency.
    But In current implementation the hardware initialization function is
    called much before, the operating frequency information is available to
    the driver. Due to this at higher frequencies the driver is not able to
    set the appropriate clock and data tap delay values.
    Fixed it by delaying hardware initialization, so that operating frequency
    information is available when calling the function.
    
    Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
    Acked-by: Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    State: pending
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/spi/spi-zynqmp-gqspi.c
+++ drivers/spi/spi-zynqmp-gqspi.c
@@ -1339,9 +1339,6 @@ static int zynqmp_qspi_probe(struct platform_device *pdev)
 	if (of_property_read_bool(pdev->dev.of_node, "has-io-mode"))
 		xqspi->io_mode = true;
 
-	/* QSPI controller initializations */
-	zynqmp_qspi_init_hw(xqspi);
-
 	pm_runtime_mark_last_busy(&pdev->dev);
 	pm_runtime_put_autosuspend(&pdev->dev);
 	xqspi->irq = platform_get_irq(pdev, 0);
@@ -1400,6 +1397,9 @@ static int zynqmp_qspi_probe(struct platform_device *pdev)
 	ctlr->dev.of_node = np;
 	xqspi->speed_hz = ctlr->max_speed_hz;
 	ctlr->auto_runtime_pm = true;
+
+	/* QSPI controller initializations */
+	zynqmp_qspi_init_hw(xqspi);
 
 	ret = devm_spi_register_controller(&pdev->dev, ctlr);
 	if (ret) {
