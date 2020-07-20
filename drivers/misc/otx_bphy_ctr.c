16add4c995b8f75c4b575fc38b12da641abe210c
Ruiqiang Hao <Ruiqiang.Hao@windriver.com>
2020-07-20 05:44:37 +0000

    misc: otx_bphy_ctr: Fix access_ok() argument error
    Commit ("96d4f267e40f Remove 'type' argument from access_ok() function")
    changed the definition of access_ok.
    
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/misc/otx_bphy_ctr.c
+++ drivers/misc/otx_bphy_ctr.c
@@ -120,11 +120,9 @@ static long otx_dev_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
 		return -ENOTTY;
 
 	if (_IOC_DIR(cmd) & _IOC_READ)
-		err = !access_ok(VERIFY_WRITE, (void __user *)arg,
-				 _IOC_SIZE(cmd));
+		err = !access_ok((void __user *)arg, _IOC_SIZE(cmd));
 	else if (_IOC_TYPE(cmd) & _IOC_WRITE)
-		err = !access_ok(VERIFY_READ, (void __user *)arg,
-				 _IOC_SIZE(cmd));
+		err = !access_ok((void __user *)arg, _IOC_SIZE(cmd));
 
 	if (err)
 		return -EFAULT;
