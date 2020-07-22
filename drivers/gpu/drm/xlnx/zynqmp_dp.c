a4fc026e9f402057145e7585aa26cce2b28834a4
Hyun Kwon <hyun.kwon@xilinx.com>
2020-07-22 10:08:34 -0700

    drm: xlnx: zynqmp: fix -Wunused-but-set-variable warnings
    commit  79e2368a6bd8a3036c40d7feb82ed7b12fe31851 from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v5.4
    
    Fix all warnings such as below.
    
    drivers/gpu/drm/xlnx/zynqmp_dpsub.c: In function 'zynqmp_dpsub_remove':
    drivers/gpu/drm/xlnx/zynqmp_dpsub.c:127:11: warning: variable 'ret' set but not used [-Wunused-but-set-variable]
    ...
    
    Signed-off-by: Hyun Kwon <hyun.kwon@xilinx.com>
    Reviewed-by: Dylan Yip <dylan.yip@xilinx.com>
    State: pending
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/gpu/drm/xlnx/zynqmp_dp.c
+++ drivers/gpu/drm/xlnx/zynqmp_dp.c
@@ -1460,7 +1460,6 @@ zynqmp_dp_connector_atomic_set_property(struct drm_connector *connector,
 					uint64_t val)
 {
 	struct zynqmp_dp *dp = connector_to_dp(connector);
-	int ret;
 
 	if (property == dp->sync_prop) {
 		zynqmp_dp_set_sync_mode(dp, val);
@@ -1471,7 +1470,7 @@ zynqmp_dp_connector_atomic_set_property(struct drm_connector *connector,
 		if (bpc) {
 			drm_object_property_set_value(&connector->base,
 						      property, bpc);
-			ret = -EINVAL;
+			return -EINVAL;
 		}
 	} else {
 		return -EINVAL;
