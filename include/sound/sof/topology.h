20b4f2f7139751bbf10d16cf3d5d9d5ba9f9a28c
Daniel Baluta <daniel.baluta@nxp.com>
2020-08-17 18:42:31 +0300

    MLK-24255-1 ASoC: SOF: Add missing comp/process types
    commit 3c7733829c156c843070a588cf5359adc02c5c08 from
    https://source.codeaurora.org/external/imx/linux-imx
    
    This aligns SOF firmware with kernel making place for
    safely introducing Post Process Component.
    
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>

--- include/sound/sof/topology.h
+++ include/sound/sof/topology.h
@@ -39,6 +39,8 @@ enum sof_comp_type {
 	SOF_COMP_ASRC,		/**< Asynchronous sample rate converter */
 	SOF_COMP_DCBLOCK,
 	SOF_COMP_SMART_AMP,             /**< smart amplifier component */
+	SOF_COMP_ASRC,
+	SOF_COMP_DCBLOCK,
 	/* keep FILEREAD/FILEWRITE as the last ones */
 	SOF_COMP_FILEREAD = 10000,	/**< host test based file IO */
 	SOF_COMP_FILEWRITE = 10001,	/**< host test based file IO */
