a89b854a08fce3a20f91d85638c3d4af999ec0ba
Geetha sowjanya <gakula@marvell.com>
2020-10-22 00:06:28 +0530

    octeontx2-pf: cn10k: Add packet processing support
    commit 5032dde2d81adc65d7ca519fd179d81f8e45e632 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    This patch adds support to use new LMTST lines for NPA batch free
    and burst SQE flush. Adds new dev_hw_ops structure to hold platform
    specific functions and create new files cn10k.c and cn10k.h.
    
    Change-Id: I439ced355ad768200431b8db7a889cfab91b83ba
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    [RQ: Replace otx2_alloc_rbuf() with __otx2_alloc_rbuf().]
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_txrx.h
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_txrx.h
@@ -116,6 +116,7 @@ struct otx2_pool {
 	struct qmem		*stack;
 	struct qmem		*fc_addr;
 	u16			rbsize;
+	u64			*lmt_addr;
 };
 
 struct otx2_cq_queue {
@@ -157,4 +158,10 @@ static inline u64 otx2_iova_to_phys(void *iommu_domain, dma_addr_t dma_addr)
 int otx2_napi_handler(struct napi_struct *napi, int budget);
 bool otx2_sq_append_skb(struct net_device *netdev, struct otx2_snd_queue *sq,
 			struct sk_buff *skb, u16 qidx);
+void cn10k_sqe_flush(void *dev, struct otx2_snd_queue *sq,
+		     int size, int qidx);
+void otx2_sqe_flush(void *dev, struct otx2_snd_queue *sq,
+		    int size, int qidx);
+void otx2_refill_pool_ptrs(void *dev, struct otx2_cq_queue *cq);
+void cn10k_refill_pool_ptrs(void *dev, struct otx2_cq_queue *cq);
 #endif /* OTX2_TXRX_H */
