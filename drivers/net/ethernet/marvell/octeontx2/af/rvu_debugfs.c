521c72fc5f09d66179be27ca589e6811b5088476
Subbaraya Sundeep <sbhatta@marvell.com>
2020-12-11 14:12:53 +0530

    octeontx2-af: Check for no CGX devices
    commit e7eaf5f2d2f53a685ab112c0d4bf99fd19983fa2 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    It is possible that there are no CGX devices
    in the system. This patch ensures that code
    is checked for no CGX devices wherever required.
    
    Change-Id: I5ac7d0a4cb6077c29c3aef1c83fb175601a08f08
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/42226
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_debugfs.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_debugfs.c
@@ -2850,6 +2850,9 @@ static int cgx_print_stats(struct seq_file *s, int lmac_id)
 		return -ENODEV;
 
 	mac_ops = cgx_get_mac_ops(cgxd);
+	/* There can be no CGX devices at all */
+	if (!mac_ops)
+		return 0;
 
 	/* Link status */
 	seq_puts(s, "\n=======Link Status======\n\n");
@@ -2973,6 +2976,9 @@ static void rvu_dbg_cgx_init(struct rvu *rvu)
 		return;
 
 	mac_ops = cgx_get_mac_ops(rvu_cgx_pdata(rvu_def_cgx_id, rvu));
+	/* There can be no CGX devices at all */
+	if (!mac_ops)
+		return;
 	rvu->rvu_dbg.cgx_root = debugfs_create_dir(mac_ops->name,
 						   rvu->rvu_dbg.root);
 
@@ -3379,6 +3385,9 @@ void rvu_dbg_init(struct rvu *rvu)
 	if (!pfile)
 		goto create_failed;
 
+	if (!cgx_get_cgxcnt_max())
+		goto create;
+
 	if (is_rvu_otx2(rvu))
 		pfile = debugfs_create_file("rvu_pf_cgx_map", 0444, rvu->rvu_dbg.root,
 					    rvu, &rvu_dbg_rvu_pf_cgx_map_fops);
@@ -3388,6 +3397,7 @@ void rvu_dbg_init(struct rvu *rvu)
 	if (!pfile)
 		goto create_failed;
 
+create:
 	rvu_dbg_npa_init(rvu);
 	rvu_dbg_nix_init(rvu, BLKADDR_NIX0);
 	rvu_dbg_nix_init(rvu, BLKADDR_NIX1);
