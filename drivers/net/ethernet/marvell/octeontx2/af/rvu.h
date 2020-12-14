5b186946aba7db5ba6532d29eca80058b34f4e7a
Ruiqiang Hao <Ruiqiang.Hao@windriver.com>
2020-12-14 08:43:58 +0000

    driver: net: marvell: Fixed warning caused by inconsistent const attribute
    commit 42006910b5ea(octeontx2-af: cleanup KPU config data) defined some
    variable as const,we should change formal parameters which not be changed
    in function as const too.
    
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.h
@@ -446,7 +446,7 @@ struct npc_kpu_profile_adapter {
 	const struct npc_lt_def_cfg	*lt_def;
 	const struct npc_kpu_profile_action	*ikpu; /* array[pkinds] */
 	const struct npc_kpu_profile	*kpu; /* array[kpus] */
-	const struct npc_mcam_kex	*mkex;
+	struct npc_mcam_kex	*mkex;
 	bool				custom; /* true if loadable profile used */
 	size_t				pkinds;
 	size_t				kpus;
