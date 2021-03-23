60da48d3d12f2bf4459ed0f23bce25fd25ebe648
Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
2021-03-22 23:29:57 -0600

    mtd: spi-nor: Fix flash unlocking sequence for sst flash parts
    commit  2e48bdc72631c13acbec561f4cd97bf0c8842014 from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v5.10
    
    In few sst flash parts(sst26wf016B) call trace was observed while unlocking
    the flash memory.
    This patch fixes the unlocking squence.
    
    Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
    State: pending
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/mtd/spi-nor/core.c
+++ drivers/mtd/spi-nor/core.c
@@ -3364,11 +3364,11 @@ static void spi_nor_try_unlock_all(struct spi_nor *nor)
 
 	if (info->flags & SST_GLOBAL_PROT_UNLK) {
 		spi_nor_prot_unlock(nor);
+	} else {
+		ret = spi_nor_unlock(&nor->mtd, 0, nor->params->size);
+		if (ret)
+			dev_dbg(nor->dev, "Failed to unlock the entire flash memory array\n");
 	}
-
-	ret = spi_nor_unlock(&nor->mtd, 0, nor->params->size);
-	if (ret)
-		dev_dbg(nor->dev, "Failed to unlock the entire flash memory array\n");
 }
 
 static int spi_nor_init(struct spi_nor *nor)
