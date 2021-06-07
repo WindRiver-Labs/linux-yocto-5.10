c66a5900f13998f838c533ff410028ff4aed12c9
Bruce Ashfield <bruce.ashfield@gmail.com>
2021-06-07 08:23:42 -0400

    Revert "Revert "aufs: initial port to v5.10""
    This reverts commit 64e145dcca8ca85e11a8f1de01b1fe9d69176420.

--- fs/aufs/vfsub.c
+++ fs/aufs/vfsub.c
@@ -525,10 +525,10 @@ ssize_t vfsub_read_k(struct file *file, void *kbuf, size_t count,
 	} buf;
 
 	buf.k = kbuf;
-	oldfs = get_fs();
-	set_fs(KERNEL_DS);
+	oldfs = force_uaccess_begin();
+	force_uaccess_end(oldfs);
 	err = vfsub_read_u(file, buf.u, count, ppos);
-	set_fs(oldfs);
+	force_uaccess_end(oldfs);
 	return err;
 }
 
@@ -555,10 +555,10 @@ ssize_t vfsub_write_k(struct file *file, void *kbuf, size_t count, loff_t *ppos)
 	} buf;
 
 	buf.k = kbuf;
-	oldfs = get_fs();
-	set_fs(KERNEL_DS);
+	oldfs = force_uaccess_begin();
+	force_uaccess_end(oldfs);
 	err = vfsub_write_u(file, buf.u, count, ppos);
-	set_fs(oldfs);
+	force_uaccess_end(oldfs);
 	return err;
 }
 
