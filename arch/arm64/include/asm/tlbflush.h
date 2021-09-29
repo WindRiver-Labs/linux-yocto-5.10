b1034f052c2bbc9cc44f8d1f19a0b1dc39c11a65
Meng Li <Meng.Li@windriver.com>
2021-09-29 21:14:44 +0800

    arm64: mm: add the code that is missed when upgrade kernel
    When booting up on nxp ls/lx series platforms, system hangs
    at init process. This issue caused by merging upstream commit
    5e10f9887ed8("arm64: mm: Fix TLBI vs ASID rollover"). Because
    some code is missed when there is conflict with sdk patch.
    Therefore, add the missing code manually.
    
    Signed-off-by: Meng Li <Meng.Li@windriver.com>
    Signed-off-by: Bruce Ashfield <bruce.ashfield@gmail.com>

--- arch/arm64/include/asm/tlbflush.h
+++ arch/arm64/include/asm/tlbflush.h
@@ -256,6 +256,7 @@ static inline void flush_tlb_mm(struct mm_struct *mm)
 		dsb(ish);
 		isb();
 	} else {
+		asid = __TLBI_VADDR(0, ASID(mm));
 		__tlbi(aside1is, asid);
 		__tlbi_user(aside1is, asid);
 		dsb(ish);
@@ -274,6 +275,7 @@ static inline void flush_tlb_page_nosync(struct vm_area_struct *vma,
 		dsb(ish);
 		isb();
 	} else {
+		addr = __TLBI_VADDR(uaddr, ASID(vma->vm_mm));
 		__tlbi(vale1is, addr);
 		__tlbi_user(vale1is, addr);
 	}
