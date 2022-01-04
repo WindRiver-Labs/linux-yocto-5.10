402199de97c392fedbbfaec3fff5470230768c1c
Hariprasad Kelam <hkelam@marvell.com>
2022-01-04 15:44:53 +0530

    octeontx2-pf: cn10k: add ptp 1-step capability
    commit 9c18dc0b55310aa52497207cd6dd6e009df33f78 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    This patch adds 1-step PTP support for RVU netdev on CN10K silicon
    
    Change-Id: I7c160a02d8fecf8b3528c47415a1ccb217094471
    Signed-off-by: Hariprasad Kelam <hkelam@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/69819
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_nix.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_nix.c
@@ -4462,8 +4462,14 @@ static int rvu_nix_block_init(struct rvu *rvu, struct nix_hw *nix_hw)
 	/* Restore CINT timer delay to HW reset values */
 	rvu_write64(rvu, blkaddr, NIX_AF_CINT_DELAY, 0x0ULL);
 
+	cfg = rvu_read64(rvu, blkaddr, NIX_AF_SEB_CFG);
+
 	/* For better performance use NDC TX instead of NDC RX for SQ's SQEs" */
-	rvu_write64(rvu, blkaddr, NIX_AF_SEB_CFG, 0x1ULL);
+	cfg |= 1ULL;
+	if (!is_rvu_otx2(rvu))
+		cfg |= NIX_PTP_1STEP_EN;
+
+	rvu_write64(rvu, blkaddr, NIX_AF_SEB_CFG, cfg);
 
 	if (!is_rvu_otx2(rvu))
 		rvu_nix_block_cn10k_init(rvu, nix_hw);
