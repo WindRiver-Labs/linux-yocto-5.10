1be888d85e6722abb1170ebcec538f120d7e341c
Subbaraya Sundeep <sbhatta@marvell.com>
2022-04-04 18:35:10 +0530

    octeontx2-pf: Do not set rbuf len as zero
    commit 7fc5361175414fd0ab75a19c4b9b53c5246afd79 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    When MTU is changed then receive buffer length set
    by user is zeroed currently. Change this behavior
    by retaining receive buffer length. This is suggested
    by upstream maintainer.
    
    Change-Id: I03e1cb1b4e28a28157df2fa492ecd6ca8c1a8390
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/74555
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
@@ -53,7 +53,6 @@ static int otx2_config_hw_rx_tstamp(struct otx2_nic *pfvf, bool enable);
 
 static int otx2_change_mtu(struct net_device *netdev, int new_mtu)
 {
-	struct otx2_nic *pf = netdev_priv(netdev);
 	bool if_up = netif_running(netdev);
 	int err = 0;
 
@@ -63,10 +62,6 @@ static int otx2_change_mtu(struct net_device *netdev, int new_mtu)
 	netdev_info(netdev, "Changing MTU from %d to %d\n",
 		    netdev->mtu, new_mtu);
 	netdev->mtu = new_mtu;
-	/* Modify receive buffer size based on MTU and do not
-	 * use the fixed size set.
-	 */
-	pf->hw.rbuf_fixed_size = 0;
 
 	if (if_up)
 		err = otx2_open(netdev);
@@ -1342,7 +1337,8 @@ static int otx2_get_rbuf_size(struct otx2_nic *pf, int mtu)
 	int rbuf_size;
 
 	if (pf->hw.rbuf_fixed_size)
-		return pf->hw.rbuf_fixed_size;
+		return ALIGN(pf->hw.rbuf_fixed_size, OTX2_ALIGN) +
+			OTX2_HEAD_ROOM;
 
 	/* The data transferred by NIX to memory consists of actual packet
 	 * plus additional data which has timestamp and/or EDSA/HIGIG2
@@ -2687,6 +2683,7 @@ static int otx2_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	hw->tx_queues = qcount;
 	hw->tot_tx_queues = qcount;
 	hw->max_queues = qcount;
+	hw->rbuf_fixed_size = OTX2_DEFAULT_RBUF_LEN;
 	/* Use CQE of 128 byte descriptor size by default */
 	hw->xqe_size = 128;
 
